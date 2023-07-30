d517d3f8b34cac0147e5e99d42e2b400bb833c57
Josué Natanael Cardoso de Freitas <josue.n.freitas@inesctec.pt>
2023-07-30 16:21:23 +0100

    Project setup

---
+++ backend/src/test/java/josuefreitas/interactfusion/InteractFusion/InteractFusionApplicationTests.java
@@ -0,0 +1,13 @@
+package josuefreitas.interactfusion.InteractFusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractFusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ frontend/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import "./styles.css"; // Import Tailwind CSS
+
+function App() {
+    return <div className="bg-green-500 text-white p-4 rounded-md">Tailwind CSS!</div>;
+}
+
+export default App;
